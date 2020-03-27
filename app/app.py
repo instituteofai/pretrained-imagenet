2a396a59056295efebb5d91c9e333028375d6864
Chandan Kumar <chandankumar99341@gmail.com>
2020-03-27 09:14:04 +0530

    Add image upload via url

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 from PIL import Image
 import io
 from keras.preprocessing.image import img_to_array
+import urllib.request
+from socket import timeout
+from urllib.error import HTTPError, URLError
 
 import classifier as C
 
@@ -23,15 +26,36 @@ def index():
   elif request.method == "POST":
 
     f = request.files['file']
-    img = f.read()
-    img = Image.open(io.BytesIO(img))
-    img = img_to_array(img)
+    errorMessage = None
+    imgUrl = request.form.get('imgUrl')
+    if f:
+      print('file is uploaded!')
+      img = f.read()
+      img = Image.open(io.BytesIO(img))
+    elif imgUrl:
+      print('image url is given!')
+      try:
+        img = Image.open(urllib.request.urlopen(imgUrl, timeout=15))
+      except (HTTPError, URLError) as error:
+        errorMessage = error
+      except timeout:
+        errorMessage = 'Timed out while reading the image!'
+      else:
+        print('Image read successfully!')
+      
+    else:
+      return render_template('index.html', data={ 'message': 'No files were given!' })
+    
+    if (errorMessage is not None):
+      return render_template('index.html', data={'message': errorMessage })
+    else:
+      img = img_to_array(img)
+      results = C.predict(img)
 
-    results = C.predict(img)
+      data = {
+        'results': results,
+        'errors': []
+      }
 
-    data = {
-      'results': results,
-      'errors': []
-    }
-
-    return render_template('index.html', data=data)
+      return render_template('index.html', data=data)
+      
